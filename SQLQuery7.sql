b6eca67e9a0070598a8de272991ce7d3ae00ea19
rafita <151587237+bruxa61@users.noreply.github.com>
2024-10-07 14:11:34 -0300

    CORREÇÃO

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,14 +1,14 @@
---QUEST√O 1
+--QUEST√ÉO 1
 --SELECT * 
 --FROM DimProduct
---A: N˙mero de produtos continua 2.517
+--A: N√∫mero de produtos continua 2.517
 
 --SELECT *
 --FROM DimCustomer
 --B: A quantidade de clientes diminuiu, caindo para 18.869
 
 
---QUEST√O 2
+--QUEST√ÉO 2
 --A
 --SELECT CustomerKey, FirstName, EmailAddress, BirthDate
 --FROM dimCustomer
@@ -18,10 +18,10 @@
 --CustomerKey AS 'Chave Cliente',
 --FirstName AS Nome,
 --EmailAddress AS Email,
---BirthDate AS Anivers·rio
+--BirthDate AS Anivers√°rio
 --FROM dimCustomer
 
---QUEST√O 3
+--QUEST√ÉO 3
 --A
 --SELECT TOP(100) *
 --FROM dimCustomer
@@ -38,18 +38,18 @@
 --SELECT
 --FirstName AS 'Primeiro Nome',
 --EmailAddress AS 'E-mail',
---BirthDate AS Anivers·rio
+--BirthDate AS Anivers√°rio
 --FROM dimCustomer
 
---QUEST√O 4
+--QUEST√ÉO 4
 --SELECT Manufacturer
 --FROM DimProduct
 
---SELECT 
+--SELECT DISTINCT
 --Manufacturer AS Fornecedor
 --FROM DimProduct
 
---QUEST√O 5
+--QUEST√ÉO 5
 --SELECT DISTINCT ProductName
 --FROM DimProduct
 --2.517
@@ -58,4 +58,4 @@
 --FROM FactSales
 --2.516
 
---R: Um produto n„o foi vendido
+--R: Um produto n√£o foi vendido

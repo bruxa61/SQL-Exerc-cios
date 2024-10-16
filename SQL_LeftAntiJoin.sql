2912816cf519c747c8c42f3de0be9dda48c37b72
rafita <151587237+bruxa61@users.noreply.github.com>
2024-10-16 15:40:47 -0300

    apresentação

---
+++ SQL_LeftAntiJoin.sql
@@ -0,0 +1,45 @@
+--1. Primeiro:
+--(Produtos que não foram vendidos na loja online)
+SELECT 
+    ProductName, 
+	SalesAmount
+FROM 
+    DimProduct 
+LEFT JOIN 
+    FactOnlineSales ON DimProduct.ProductKey = FactOnlineSales.ProductKey
+WHERE 
+    FactOnlineSales.SalesAmount IS NULL;
+
+--2. Segundo:
+--(Produtos que não estão no inventário)
+SELECT
+	DimProduct.ProductName,
+	FactInventory.ProductKey
+FROM 
+	DimProduct
+LEFT JOIN 
+	FactInventory 
+ON 
+	DimProduct.ProductKey = FactInventory.ProductKey
+WHERE 
+	FactInventory.ProductKey IS NULL;
+
+--3. Terceiro
+--(Continentes que não tem uma chave geográfica)
+SELECT
+ContinentName,
+DimSalesTerritory.GeographyKey
+FROM DimGeography
+LEFT JOIN
+DimSalesTerritory ON DimGeography.GeographyKey = DimSalesTerritory.GeographyKey
+WHERE DimSalesTerritory.GeographyKey IS NULL;
+
+--4. Quarto
+--(Produtos que não tem uma subcategoria)
+SELECT
+ProductName,
+DimProductSubcategory.ProductSubcategoryKey
+FROM DimProduct
+LEFT JOIN
+DimProductSubcategory ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+WHERE DimProductSubcategory.ProductSubcategoryKey IS NULL;

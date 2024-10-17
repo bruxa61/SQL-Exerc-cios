28f1e7759cfe5ba8e68f5fae379a2b4b6e29bae0
rafita <151587237+bruxa61@users.noreply.github.com>
2024-10-17 16:49:59 -0300

    Joins

---
+++ SQL05.sql
@@ -0,0 +1,40 @@
+--Questão 01
+SELECT
+ProductSubcategoryName,
+DimProductSubcategory.ProductSubcategoryKey
+FROM DimProductSubcategory
+INNER JOIN DimProduct 
+ON DimProductSubcategory.ProductSubcategoryKey = DimProduct.ProductSubcategoryKey
+
+--Questão 02
+SELECT
+ProductCategoryName,
+DimProductCategory.ProductCategoryKey
+FROM DimProductCategory
+LEFT JOIN DimProductSubcategory
+ON DimProductCategory.ProductCategoryKey = DimProductSubcategory.ProductCategoryKey
+
+--Questão 03
+SELECT
+StoreKey,
+StoreName,
+EmployeeCount,
+DimGeography.ContinentName,
+DimGeography.RegionCountryName
+--DimStore.GeographyKey
+FROM DimStore
+LEFT JOIN DimGeography
+ON Dimstore.GeographyKey = DimGeography.GeographyKey
+
+--Questão 04
+SELECT
+ProductKey,
+ProductName,
+DimProductCategory.ProductCategoryDescription,
+DimProductSubcategory.ProductCategoryKey,
+DimProduct.ProductSubcategoryKey
+FROM DimProduct
+LEFT JOIN DimProductSubcategory
+ON DimProductSubcategory.ProductSubcategoryKey = DimProduct.ProductSubcategoryKey
+LEFT JOIN DimProductCategory
+ON DimProductCategory.ProductCategoryKey = DimProductSubcategory.ProductCategoryKey
